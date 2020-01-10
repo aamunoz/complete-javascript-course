ef1d50db15fc1c86f9076e074d5e158e20dc9f64
Alberto Munoz <alberto@kinja.com>
2020-01-10 17:48:52 -0500

    Section 4 progress, completed game minus challenges

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -7,67 +7,123 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
+Challenges:
+1. A player that rolls two sixes in a row loses their entire score
+2. The winning score is configurable by the players using an input field
+3. Two dice are rolled each time. If either one rolls a one, the player's turn ends
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, isGameActive;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0; // 0 = player 1, 1 = player 2, matches array indices in score
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0; // 0 = player 1, 1 = player 2, matches array indices in score
 
-// The IDs in the HTML also use 0 and 1, so we can use activePlayer to select components related to the players
-// Also shows inserting plain text into an element
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+    // The IDs in the HTML also use 0 and 1, so we can use activePlayer to select components related to the players
+    // Also shows inserting plain text into an element
+    // document.querySelector('#current-' + activePlayer).textContent = dice;
 
-// Shows inserting HTML instead of plain text
-// document.querySelector('#current-' + activePlayer).innerHtml = '<em>' + dice + '</em>';
+    // Shows inserting HTML instead of plain text
+    // document.querySelector('#current-' + activePlayer).innerHtml = '<em>' + dice + '</em>';
 
-// Shows using querySelector to read instead of write
-// var x = document.querySelector('#score-0').textContent;
-// console.log(x);
+    // Shows using querySelector to read instead of write
+    // var x = document.querySelector('#score-0').textContent;
+    // console.log(x);
 
-// Hide the dice at the beginning of the game so it's not showing some random dice value
-document.querySelector('.dice').style.display = 'none';
+    // Hide the dice at the beginning of the game so it's not showing some random dice value
+    document.querySelector('.dice').style.display = 'none';
 
-// This is faster than querySelector
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    // Getting an element by its ID is faster than using a query selector
+    // Reset all indicators
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    var player0Panel = document.querySelector('.player-0-panel');
+    player0Panel.classList.remove('active');
+    player0Panel.classList.remove('winner');
+
+    var player1Panel = document.querySelector('.player-1-panel');
+    player1Panel.classList.remove('active');
+    player1Panel.classList.remove('winner');
+
+    player0Panel.classList.add('active');
+
+    // Set the game to active
+    isGameActive = true;
+}
+
+init();
+
+function changePlayer() {
+    // Next player
+    activePlayer = activePlayer == 0 ? 1 : 0;
+
+    // Reset round score
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // Change active player indicator. toggle works better than add/remove in this case
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 // Events can only be processed when execution stack is empty :O
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    // 1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
 
-    // 2. Display result
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+    if (isGameActive) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    // 3. Update round score IF the rolled number was NOT a 1 (rolling a 1 ends the turn)
-    if (dice != 1) {
-        // Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        // Next player
-        activePlayer = activePlayer == 0 ? 1 : 0;
+        // 2. Display result
+        var diceDom = document.querySelector('.dice');
+        diceDom.style.display = 'block';
+        diceDom.src = 'dice-' + dice + '.png';
 
-        // Reset round score
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '1';
-
-        // Change active player indicator. toggle works better than add/remove in this case
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        // 3. Update round score IF the rolled number was NOT a 1 (rolling a 1 ends the turn)
+        if (dice != 1) {
+            // Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            changePlayer();
+        }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
 
+    if (isGameActive) {
+        // Add current score to global score
+        scores[activePlayer] += roundScore;
+
+        // Update UI
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won (value may be less than 100 for testing purposes)
+        if (scores[activePlayer] >= 100) {
+            document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            
+            var winnerPanel = document.querySelector('.player-' + activePlayer + '-panel');
+            winnerPanel.classList.remove('active');
+            winnerPanel.classList.add('winner');
+
+            isGameActive = false;
+        } else {
+            // Change players if the player didn't win
+            changePlayer();
+        }
+    }
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
